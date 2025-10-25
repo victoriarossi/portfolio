ae932c72925f0bd7d421a404faa2e49a276bff95
victoriarossi <vross3@uic.edu>
2025-10-25 00:08:04 -0500

    typo

--- src/components/Lectures.js
+++ src/components/Lectures.js
@@ -6,7 +6,7 @@ const lecturesData = [
   { title: 'Introduction to Go', description: 'UIC, Fall 2025', link: 'https://drive.google.com/file/d/1Xmo-dHyV2Ooj0Q_0_MhPtftMcRXmFvgl/view?usp=sharing' },
   { title: 'Chomsky Normal Form I', description: 'UIC, Spring 2025', link: 'https://drive.google.com/file/d/1r1Roj-ypGeZpI3VbkRow6XfsBSK2YZz3/view?usp=sharing' },
   { title: 'Chomsky Normal Form II', description: 'UIC, Spring 2025', link: 'https://drive.google.com/file/d/1SnnjB4YNC39rLbWfcF5cEwCilrXMNWrJ/view?usp=sharing' },
-  { title: 'Recursion — CS 107', description: 'UIC, Fall 2024', link: 'https://drive.google.com/file/d/1YV3TqUXTrXo0OROdfFGe433zEkdvV9q5/view?usp=sharing' },
+  { title: 'Recursion', description: 'UIC, Fall 2024', link: 'https://drive.google.com/file/d/1YV3TqUXTrXo0OROdfFGe433zEkdvV9q5/view?usp=sharing' },
   { title: 'Introduction to OOP', description: 'UIC, Fall 2024', link: 'https://drive.google.com/file/d/1oCNy3NzqW9YX_2Z0XOBWTAq2XD5fjkwc/view?usp=sharing' }
 ];
 
